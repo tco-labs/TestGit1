44143a2f9af465e20175351127e686607e7e098e
tco_labs <tco_labs@yahoo.fr>
2017-06-14 10:00:48 +0200

    Hot fix 2

--- TestGit1/ViewController.swift
+++ TestGit1/ViewController.swift
@@ -13,8 +13,8 @@ class ViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
-    // Feature 1 - Hot fix 1
-    print("Hello world!...")
+    // Feature 1 - Hot fix 1 - Hot fix 2
+    print("Hello world...")
   }
 
   override func didReceiveMemoryWarning() {
