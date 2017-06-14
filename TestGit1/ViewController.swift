50cc4bc4beed82c08870d098cfe3c4bf41bd4a13
tco_labs <tco_labs@yahoo.fr>
2017-06-14 09:49:09 +0200

    Added feature 1

--- TestGit1/ViewController.swift
+++ TestGit1/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    // Feature 1
+    print("Hello world!")
   }
 
   override func didReceiveMemoryWarning() {
